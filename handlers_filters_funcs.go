1b701765338c8ebaa0ed8915b036c5fc1e8e2da9
ghostforpy <ghostformobile@gmail.com>
2025-01-20 20:52:41 +0300

    add filters

--- handlers_filters_funcs.go
+++ handlers_filters_funcs.go
@@ -25,3 +25,55 @@ func FromChannel(update Update) bool {
 	}
 	return update.EffectiveChat().Type == "channel"
 }
+
+func TextMessage(update Update) bool {
+	return update.Message.Text != ""
+}
+
+func HandleDocument(update Update) bool {
+	return update.Message.Document != nil
+}
+
+func HandlePhoto(update Update) bool {
+	return update.Message.Photo != nil
+}
+
+func HandleAudio(update Update) bool {
+	return update.Message.Audio != nil
+}
+
+func HandleVideo(update Update) bool {
+	return update.Message.Video != nil
+}
+
+func HandleVideoNote(update Update) bool {
+	return update.Message.VideoNote != nil
+}
+
+func HandleSticker(update Update) bool {
+	return update.Message.Sticker != nil
+}
+
+func HandleVoice(update Update) bool {
+	return update.Message.Voice != nil
+}
+
+func HandleVenue(update Update) bool {
+	return update.Message.Venue != nil
+}
+
+func HandleLocation(update Update) bool {
+	return update.Message.Location != nil
+}
+
+func HandleContact(update Update) bool {
+	return update.Message.Contact != nil
+}
+
+func HandleAnimation(update Update) bool {
+	return update.Message.Animation != nil
+}
+
+func HandlePremiumAnimation(update Update) bool {
+	return update.Message.PremiumAnimation != nil
+}
